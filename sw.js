2323c11f2735ece9b56a0b47f59e2a13ac50390c
PEYOITE <peyoite233@outlook.com>
2025-07-27 20:59:26 +0800

    Site updated: 2025-07-27 20:59:26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1753620131531";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1753621158494";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
